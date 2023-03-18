ed123d8071cb0f09c31cc98878f653d4bea9bb46
0xZonaV <zonavofficial@gmail.com>
2023-03-18 13:52:58 +1000

    Improve speed of bot

--- seibot.js
+++ seibot.js
@@ -10,61 +10,61 @@ import { calculateFee  } from "@cosmjs/stargate";
 export const seiFunctionBot = async (newMnemonics) => {
     const restEndPoint = 'https://rest.atlantic-2.seinetwork.io/';
     const rpcEndPoint = 'https://rpc.atlantic-2.seinetwork.io/';
+    const fee = calculateFee(100000, "0.02usei");
 
-    for (let i=0;i<=newMnemonics.length; i+=2) {
-        const wallet1 = await DirectSecp256k1HdWallet.fromMnemonic(
-            newMnemonics[i],
-            {prefix: 'sei'}
-        );
+    for (let j=0;j<=50;j++) {
+        const transactions = [];
 
-        const wallet2 = await DirectSecp256k1HdWallet.fromMnemonic(
-            newMnemonics[i+1],
-            {prefix: 'sei'}
-        );
+        for (let i = 0; i <= newMnemonics.length; i += 2) {
+            try {
+                const wallet1 = await DirectSecp256k1HdWallet.fromMnemonic(
+                    newMnemonics[i],
+                    {prefix: 'sei'}
+                );
 
+                const wallet2 = await DirectSecp256k1HdWallet.fromMnemonic(
+                    newMnemonics[i + 1],
+                    {prefix: 'sei'}
+                );
 
-        const [firstAccountWallet1] = await wallet1.getAccounts();
-        const [firstAccountWallet2] = await wallet2.getAccounts();
+                const [firstAccountWallet1] = await wallet1.getAccounts();
+                const [firstAccountWallet2] = await wallet2.getAccounts();
 
-        const queryClient  = await getQueryClient(restEndPoint);
+                const queryClient = await getQueryClient(restEndPoint);
 
-        const client1 = await getSigningClient(rpcEndPoint, wallet1);
-        const client2 = await getSigningClient(rpcEndPoint, wallet2);
+                const client1 = await getSigningClient(rpcEndPoint, wallet1);
+                const client2 = await getSigningClient(rpcEndPoint, wallet2);
 
-        const fee = calculateFee(100000, "0.02usei");
+                const wallet1Balances = await queryClient.cosmos.bank.v1beta1
+                    .allBalances({address: firstAccountWallet1.address});
 
+                const wallet2Balances = await queryClient.cosmos.bank.v1beta1
+                    .allBalances({address: firstAccountWallet2.address});
 
-        for (let i = 0; i<50; i++) {
-           try {
-               const wallet1Balances = await queryClient.cosmos.bank.v1beta1
-                   .allBalances({ address: firstAccountWallet1.address });
+                const wallet1SeiBalance = wallet1Balances.balances.find(token => token.denom === 'usei');
+                const wallet2SeiBalance = wallet2Balances.balances.find(token => token.denom === 'usei');
 
-               const wallet2Balances = await queryClient.cosmos.bank.v1beta1
-                   .allBalances({ address: firstAccountWallet2.address });
+                if (parseInt(wallet1SeiBalance?.amount) > 2001) {
+                    const amount = {
+                        amount: String(parseInt(wallet1SeiBalance.amount) - 2000),
+                        denom: wallet1SeiBalance.denom
+                    };
+                    transactions.push(client1.sendTokens(firstAccountWallet1.address, firstAccountWallet2.address, [amount], fee));
+                } else {
+                    const amount = {
+                        amount: String(parseInt(wallet2SeiBalance.amount) - 2000),
+                        denom: wallet2SeiBalance.denom
+                    };
+                    transactions.push(client2.sendTokens(firstAccountWallet2.address, firstAccountWallet1.address, [amount], fee));
+                }
 
-               const wallet1SeiBalance = wallet1Balances.balances.find(token => token.denom === 'usei');
-               const wallet2SeiBalance = wallet2Balances.balances.find(token => token.denom === 'usei');
-
-               if (parseInt(wallet1SeiBalance?.amount) > 2001) {
-                   const amount = { amount: String(parseInt(wallet1SeiBalance.amount)-2000), denom: wallet1SeiBalance.denom };
-
-                   const resp = await client1.sendTokens(firstAccountWallet1.address, firstAccountWallet2.address, [amount], fee);
-
-                   console.log('TX Wallet1:', resp);
-               } else {
-                   const amount = { amount: String(parseInt(wallet2SeiBalance.amount)-2000), denom: wallet2SeiBalance.denom };
-
-                   const resp = await client2.sendTokens(firstAccountWallet2.address, firstAccountWallet1.address, [amount], fee);
-
-                   console.log('TX Wallet2:', resp);
-               }
-           } catch (error) {
-               console.log("Error: ", error.message, ' in wallets: ', firstAccountWallet1.address,' ', firstAccountWallet2.address);
-           }
-
+                console.log(`Done wallets: ${i} ${i + 1}`);
+            } catch (err) {
+                console.log(err);
+            }
         }
 
-        console.log(`Done wallets: ${i} ${i+1}`);
+        await Promise.all(transactions);
     }
 }
 
